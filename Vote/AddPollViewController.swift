cdd227d676c8ea3300a70c85778542a0678db698
jhaff <jacobhaff2@gmail.com>
2018-11-10 22:40:44 -0800

    Added image picker to AddPollViewController

--- Vote/AddPollViewController.swift
+++ Vote/AddPollViewController.swift
@@ -11,16 +11,44 @@ import UIKit
 
 class AddPollViewController: UIViewController {
     
+    // MARK: Outlets
+
     @IBOutlet weak var topImageView: UIImageView!
-    
     @IBOutlet weak var bottomImageView: UIImageView!
     @IBOutlet weak var navBar: UINavigationBar!
     
+    private var selectedImage: UIImage?
+    private var selectedImageView: UIImageView?
     
+    // MARK: Actions
     
+    @IBAction func topImageTapped(_ sender: UITapGestureRecognizer) {
+        displayImagePicker()
+        
+        topImageView.image = selectedImage
+        selectedImageView = topImageView
+    }
+    
+    @IBAction func bottomImageTapped(_ sender: UITapGestureRecognizer) {
+        displayImagePicker()
+        
+        bottomImageView.image = selectedImage
+        selectedImageView = bottomImageView
+    }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    // MARK: Private Functions
+    
+    private func displayImagePicker(){
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        self.present(imagePicker, animated: true, completion: nil)
         
     }
 
@@ -37,4 +65,18 @@ class AddPollViewController: UIViewController {
         super.viewDidLoad()
         setupViews()
     }
+}
+
+extension AddPollViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.editedImage] as? UIImage{
+            selectedImageView?.image = selectedImage
+        } else {
+            selectedImageView?.image = info[.originalImage] as? UIImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
