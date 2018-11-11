02f1f7d9b8a8f1107ea1434ddae97e6091a904d1
jhaff <jacobhaff2@gmail.com>
2018-11-10 21:34:15 -0800

    Built UI of AddPoll page

--- Vote/AddPollViewController.swift
+++ Vote/AddPollViewController.swift
@@ -1,34 +1,40 @@
 //
-//  ViewController.swift
+//  AddPollViewController.swift
 //  Vote
 //
 //  Created by Jacob Haff on 11/10/18.
 //  Copyright © 2018 Jacob Haff. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class AddPollViewController: UIViewController {
+    
     @IBOutlet weak var topImageView: UIImageView!
     
     @IBOutlet weak var bottomImageView: UIImageView!
+    @IBOutlet weak var navBar: UINavigationBar!
     
+    
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+    }
+
     private func setupViews(){
         topImageView.layer.cornerRadius = 22
         bottomImageView.layer.cornerRadius = 22
         topImageView.layer.applySketchShadow(color: .black, alpha: 0.2, x: 0, y: 10, blur: 11)
         bottomImageView.layer.applySketchShadow(color: .black, alpha: 0.2, x: 0, y: 10, blur: 11)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
+            navBar.shadowImage = UIImage()
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         setupViews()
-        
     }
-
-
 }
-
